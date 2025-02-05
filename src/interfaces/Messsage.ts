5f124e6fde1c18a03547adeb6a443c5f6350ce1d
Mukul2244 <mukulsharma@1594@gmail.com>
2025-02-05 14:00:58 +0530

    Add TypeScript interfaces for Message and User

---
+++ src/interfaces/Messsage.ts
@@ -0,0 +1,13 @@
+export interface MessageInterface {
+    id: number;
+    text: string;
+    sender_username: string;
+} 
+
+// "id": 141,
+// "text": "aaa",
+// "timestamp": "2025-01-23T17:16:52.598269+05:30",
+// "sender": 6,
+// "restaurant": 1,
+// "sender_username": "abc",
+// "restaurant_name": "restraunt 1"
---
+++ src/interfaces/User.ts
@@ -0,0 +1,9 @@
+export interface UserInterface{
+    id: string
+    username: string
+    avatar?: string
+}
+// interface User {
+//     id: string
+//     username: string
+//   }
